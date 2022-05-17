18cda22731e0b281eb6c37ae58c9783aacf4d001
Crowdbotics <team@crowdbotics.com>
2022-05-17 18:27:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "shawarmania_35252",
+  url: "https://shawarmania_35252.botics.co",
+  api: "https://shawarmania_35252.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
